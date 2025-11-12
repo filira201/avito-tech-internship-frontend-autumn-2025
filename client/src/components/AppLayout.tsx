ae36d4b03ab9af172c9210c3234e108ec6007ee5
Egor <egor297198@mail.ru>
2025-11-12 18:41:09 +0400

    Add: add Footer component

--- client/src/components/AppLayout.tsx
+++ client/src/components/AppLayout.tsx
@@ -1,5 +1,7 @@
 import { Outlet } from "react-router";
 
+import { Footer } from "./Footer";
+
 export const AppLayout = () => {
   return (
     <div className="flex flex-col min-h-screen h-full  w-full">
@@ -7,7 +9,7 @@ export const AppLayout = () => {
       <main className="flex-1 mx-auto max-w-[1252px] w-full p-[10px_round(up,1.22223%,.2rem)] sm:p-[15px_round(up,3.22223%,.2rem)] lg:p-[20px_round(up,7.22223%,.2rem)]">
         <Outlet />
       </main>
-      <footer>Footer</footer>
+      <Footer />
     </div>
   );
 };
